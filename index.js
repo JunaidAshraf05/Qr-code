b0fe59f458249a4c2edc93be44d11a8625da0dd1
sStudent <junaidashraf2500@gmail.com>
2024-10-26 21:23:53 +0530

    Qr code

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
 const wrapper = document.querySelector(".wrapper"),
 qrInput = wrapper.querySelector(".form input"),
 generateBtn = wrapper.querySelector(".form button"),
-qrImg = wrapper.querySelector(".qr code img");
+qrImg = wrapper.querySelector(".qr-code img");
+let preValue;
 
-let prevalue;
-
-generateBtn.addEventListener("click",()=>{
-     let qrValue = qrInput.value.trim();
-     if(!qrValue || prevalue ===qrValue)return;
-     prevalue = qrValue;
-     generateBtn.innerText = "Generating Qr code....";
-     qrImg.src=" https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=$(qrValue)"
-     qrImg.addEventListener("load",()=>{
-       wrapper.classList.add("active");
-     generateBtn.innerText = "Generate Qr code";
-     });
-
-
-});
-qrInput.addEventListener("keyup",()=>{
-    if(!qrInput.value.trim()){
-        wrapper.classList.remove("active");
-    }
+generateBtn.addEventListener("click", () => {
+    let qrValue = qrInput.value.trim();
+    if(!qrValue || preValue === qrValue) return;
+    preValue = qrValue;
+    generateBtn.innerText = "Generating QR Code...";
+    qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${qrValue}`;
+    qrImg.addEventListener("load", () => {
+        wrapper.classList.add("active");
+        generateBtn.innerText = "Generate QR Code";
     });
+});
+
+qrInput.addEventListener("keyup", () => {
+    if(!qrInput.value.trim()) {
+        wrapper.classList.remove("active");
+        preValue = "";
+    }
+});
